2d67e7d72edf5efb2ed41f652870d8a82cedef31
dbice12345 <dbice12345@gmail.com>
2024-11-05 05:12:58 +0300

    trfhjkdtyj

--- WebappDeployment.ps1
+++ WebappDeployment.ps1
@@ -4,4 +4,11 @@ New-AzResourceGroup -Name $rg -Location eastus -Force
 New-AzResourceGroupDeployment `
     -ResourceGroupName $rg `
     -TemplateFile 'template.json' `
-    -userdefined_location "centralus"
+    -mode Complete
+
+
+    Start-Sleep -Seconds 40
+
+    New-AzResourceGroupDeployment `
+    -ResourceGroupName $rg `
+    -TemplateFile 'template2.json'
