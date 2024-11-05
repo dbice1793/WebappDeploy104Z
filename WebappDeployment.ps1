5587b9a113d29ea20f995eb73e2990866a280e71
dbice12345 <dbice12345@gmail.com>
2024-11-05 07:29:15 +0300

    qwdqwd

--- WebappDeployment.ps1
+++ WebappDeployment.ps1
@@ -1,7 +1,7 @@
 $rg = 'Webappaz104'
-New-AzResourceGroup -Name $rg -Location eastus -Force
+New-AzResourceGroup -Name $rg -Location westus -Force
 
 New-AzResourceGroupDeployment `
     -ResourceGroupName $rg `
     -TemplateFile 'template.json' `
-    -userdefined_location "centralus"
+    -userdefined_location "westus"
