e667c9c6df76df092a00b57378a65bc6d4750ad4
dbice12345 <dbice12345@gmail.com>
2024-11-05 03:11:15 +0300

    Created basic webapp resource group and app deploy app

--- WebappDeploymeny.ps1
+++ WebappDeploymeny.ps1
@@ -1,10 +1,9 @@
-$rg = 'KodeKloudLinux2-centralus'
-#New-AzResourceGroup -Name $rg -Location eastus -Force
+$rg = 'Webappaz104'
+New-AzResourceGroup -Name $rg -Location eastus -Force
 
 New-AzResourceGroupDeployment `
     -Name 'CentosVM4' `
     -ResourceGroupName $rg `
     -TemplateFile '01-linuxvm.json' `
     -vmname "Centos4kk" `
-    -subnetID '/subscriptions/26bc90e3-c8d0-4345-8b20-638422194e14/resourceGroups/KodeKloudLinux2-centralus/providers/Microsoft.Network/virtualNetworks/kk2net/subnets/default' `
     -userdefined_location "centralus"
